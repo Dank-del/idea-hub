6f8016da673077e838f695c73f0873da9b6d5f27
Sayan Biswas <me@sayanbiswas.in>
2023-07-06 23:44:57 +0530

    testing next auth url
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,9 +18,26 @@ export function getBaseUrl() {
 
 export const prisma = new PrismaClient();
 
+console.log('NEXTAUTH_URL:', process.env.NEXTAUTH_URL);
+
 export const authOptions: NextAuthOptions = {
+  debug: true,
   // @ts-ignore
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async redirect(params: { url: string }) {
+      const { url } = params
+
+      // url is just a path, e.g.: /videos/pets
+      if (!url.startsWith('http')) return url
+
+      // If we have a callback use only its relative path
+      const callbackUrl = new URL(url).searchParams.get('callbackUrl')
+      if (!callbackUrl) return url
+
+      return new URL(callbackUrl as string).pathname
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -32,5 +49,5 @@ export const authOptions: NextAuthOptions = {
       }
     })
   ],
-  secret: process.env.NEXT_SECRET
+  secret: process.env.NEXTAUTH_SECRET
 };
