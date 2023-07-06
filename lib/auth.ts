cf1327a22979bb85266b3177ed2cdcde41aa4e8e
Sayan Biswas <me@sayanbiswas.in>
2023-07-06 23:51:07 +0530

    testing getBaseUrl
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- lib/auth.ts
+++ lib/auth.ts
@@ -19,25 +19,12 @@ export function getBaseUrl() {
 export const prisma = new PrismaClient();
 
 console.log('NEXTAUTH_URL:', process.env.NEXTAUTH_URL);
+console.log('BASE_URL:', getBaseUrl());
 
 export const authOptions: NextAuthOptions = {
   debug: true,
   // @ts-ignore
   adapter: PrismaAdapter(prisma),
-  callbacks: {
-    async redirect(params: { url: string }) {
-      const { url } = params
-
-      // url is just a path, e.g.: /videos/pets
-      if (!url.startsWith('http')) return url
-
-      // If we have a callback use only its relative path
-      const callbackUrl = new URL(url).searchParams.get('callbackUrl')
-      if (!callbackUrl) return url
-
-      return new URL(callbackUrl as string).pathname
-    },
-  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
