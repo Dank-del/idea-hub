9c3e553198809895f18bcd05d86a86cf5d6b27cc
Sayan Biswas <me@sayanbiswas.in>
2023-07-27 21:31:40 +0530

    shorten text in homepage idea previews
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- components/idea-card.tsx
+++ components/idea-card.tsx
@@ -16,15 +16,18 @@ export default function IdeaCard({userId, title, createdAt, content, upvotes, do
   return (
     <Card>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>Created by <span className="tracking-tighter">{user?.data?.name}</span> on {Intl.DateTimeFormat('en-US', {
+        <CardTitle>{title.substring(0, 13) + (title.length > 13 ? "...": "")}</CardTitle>
+        <CardDescription>
+          Created by
+          <span className="tracking-tighter"> {user?.data?.name} </span>
+          on {Intl.DateTimeFormat('en-US', {
           dateStyle: 'full',
           timeStyle: 'long',
           timeZone: 'Asia/Kolkata'
         }).format(new Date(createdAt))}</CardDescription>
       </CardHeader>
       <CardContent>
-        <p className="break-words">{content}</p>
+        <p className="break-words">{content.substring(0, 57).trimEnd() + (content.length > 57 ? "..." : "")}</p>
       </CardContent>
       <CardFooter>
         <div className="flex flex-row gap-2">
